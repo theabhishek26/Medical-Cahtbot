91f0cdea85da1498f863b7a5c54575fedb37e142
Abhishek Kashyap <akkashyap479@gmail.com>
2025-04-23 20:43:43 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Abhishek Kashyap',
+    author_email= 'abhishek26kashyap@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
